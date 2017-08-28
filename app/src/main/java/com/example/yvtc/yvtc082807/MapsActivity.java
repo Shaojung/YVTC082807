a7e3f54e32b4b2d4462ed3aa129f6246d07813d5
Shaojung <shaojung@gmail.com>
2017-08-28 16:11:26 +0800

    google map

--- app/src/main/java/com/example/yvtc/yvtc082807/MapsActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc082807/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -41,6 +42,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng taipei = new LatLng(25, 121.5);
+        CameraPosition tp = CameraPosition.fromLatLngZoom(taipei, 13);
+
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(tp));
+
     }
 }
